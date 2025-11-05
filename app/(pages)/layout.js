3f6d0264aa5dc5ba8b508310ecc122160e0a30a3
carledwardm <cmillardv77@gmail.com>
2025-11-05 09:57:30 -0500

    Minor changes to files

--- app/(pages)/layout.js
+++ app/(pages)/layout.js
@@ -4,8 +4,7 @@ export const metadata = {
 }
 
 export default function RootLayout ({children}) {
-    
-console.log("ABOUT LAYOUT");
+
     return (
         <html lang="en">
             <body>
--- app/layout.jsx
+++ app/layout.jsx
@@ -18,7 +18,6 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   
-console.log("ROOT LAYOUT");
   return (
     <html lang="en">
       <body>
